3f6d79d9a3f32311094887664961aabd1b674fcb
Stephen Monaco <stevemonaco@hotmail.com>
2022-11-06 23:03:00 -0500

    Add project files.

---
+++ AvaloniaApplication4/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace AvaloniaDynamicMenuTest.ViewModels;
+public partial class MainWindowViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private ObservableCollection<string> _recentFiles = new()
+    {
+        "test.txt",
+        "sample.jpg",
+        "something.cs"
+    };
+
+    [ObservableProperty] private string? _selectedFile;
+
+    [RelayCommand]
+    public void OpenRecentFileWithString(string name)
+    {
+        SelectedFile = name;
+    }
+
+    [RelayCommand]
+    public void OpenRecentFileWithObject(object name)
+    {
+        SelectedFile = (string)name;
+    }
+
+    public void OpenRecentFileWithStringMethod(string name)
+    {
+        SelectedFile = name;
+    }
+
+    public void OpenRecentFileWithObjectMethod(object name)
+    {
+        SelectedFile = (string)name;
+    }
+}
